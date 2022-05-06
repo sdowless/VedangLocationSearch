bf3b44307e90d789f46430aaf952c4953641fb07
Stephan Dowless <dowless.stephan@gmail.com>
2022-05-06 14:04:57 -0400

    Initial Commit

---
+++ VedangLocationSearch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VedangLocationSearch
+//
+//  Created by Stephen Dowless on 5/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VedangLocationSearch/VedangLocationSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  VedangLocationSearchApp.swift
+//  VedangLocationSearch
+//
+//  Created by Stephen Dowless on 5/6/22.
+//
+
+import SwiftUI
+
+@main
+struct VedangLocationSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
